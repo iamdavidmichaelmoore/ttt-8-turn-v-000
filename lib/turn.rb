66de9404681b86d3accc412a65a752b8f1d004c6
David Moore <hey@iamdavidmoore.com>
2018-04-09 00:23:09 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,3 +13,22 @@ end
 def move(array, index, value="X")
   array[index] = value
 end
+
+def valid_move?(board, index)
+
+  if position_taken?(board, index) && index.between?(0,8) == true
+    true
+  elsif position_taken?(board, index) && index.between?(0,8) == false
+    false
+  end
+end
+
+
+def position_taken?(board, index)
+  position_checked = board[index]
+  if (position_checked == " ") || (position_checked == "") || (position_checked == nil)
+    true
+  elsif (position_checked == "X") || (position_checked == "O")
+    false
+  end
+end
